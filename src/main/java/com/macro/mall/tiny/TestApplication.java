ad34744debb688c25ffdcb1cd6b8bcb54f302004
likexin <likexin>
2019-08-09 09:50:25 +0800

    mall整合SpringBoot+MyBatis搭建基本骨架

---
+++ src/main/java/com/macro/mall/tiny/TestApplication.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}

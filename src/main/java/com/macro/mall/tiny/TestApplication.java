e9b3aa8d5cfb45fe1a824fa1650791ddb09a22ac
likexin <likexin>
2019-08-23 11:58:52 +0800

    mall整合SpringSecurity和JWT实现认证和授权

--- src/main/java/com/macro/mall/tiny/TestApplication.java
+++ src/main/java/com/macro/mall/tiny/TestApplication.java
@@ -2,7 +2,6 @@ package com.macro.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.netty.channel.group.DefaultChannelGroup;
 
 @SpringBootApplication
 public class TestApplication {

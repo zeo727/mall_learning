9583518199110ba36fa727cb4aafa971a934ce1b
likexin <likexin>
2019-08-23 11:48:25 +0800

    mall整合Redis实现缓存功能

--- src/main/java/com/macro/mall/tiny/TestApplication.java
+++ src/main/java/com/macro/mall/tiny/TestApplication.java
@@ -2,6 +2,7 @@ package com.macro.mall.tiny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.netty.channel.group.DefaultChannelGroup;
 
 @SpringBootApplication
 public class TestApplication {
